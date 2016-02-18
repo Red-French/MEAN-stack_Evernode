64307a597756ca1846626e170acb50f8f196c5ba
Red-French <red@redfrench.net>
2016-02-18 10:00:08 -0600

    created new express server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 
-const app = express();
+const app = express();  // start express
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {  // on request, send response
   res.send('Server Running');
 });
 
