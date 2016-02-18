f42acf8112607f24b6430ba99e780bf0329e176e
Red-French <red@redfrench.net>
2016-02-18 17:01:19 -0600

    add body-parser and method-override

--- server.js
+++ server.js
@@ -2,38 +2,28 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+
+const logger = require('./lib/logger');
+const note = require('./routes/note');
 
 const app = express();  // start express
 const port = process.env.PORT || 3000;
-const Note = mongoose.model('Notes', mongoose.Schema({  // model
-  title: String,
-  text: String
-}));
 
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({  // for parsing form
   extended: false
 }));
+app.use(methodOverride('_method'));
+app.use(logger);
 
 app.get('/', (req, res) => {  // on request, send response
   res.send('Server Running');
 });
 
-// ROUTES
-app.get('/notes/new', (req, res) => {  // for 'new' action
-  res.render('new-note');  // serve up the form
-});
-
-app.post('/notes', (req, res) => {  // for 'post' action
-  Note.create(req.body, (err) => {
-    if (err) throw err;
-    console.log(req.body);
-    res.redirect('/');
-  });
-});
-
+app.use(note);
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
