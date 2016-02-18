21c4abbe74d2bcee80b8598f78e9425e19d58b25
Red-French <red@redfrench.net>
2016-02-18 10:36:31 -0600

    new note form

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
+
 
 const app = express();  // start express
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({  // for parsing form
+  extended: false
+}));
+
 app.get('/', (req, res) => {  // on request, send response
   res.send('Server Running');
 });
+
+// ROUTES
+app.get('/notes/new', (req, res) => {  // for 'new' action
+  res.render('new-note');  // serve up the form
+});
+
+app.post('/notes', (req, res) => {  // for 'post' action
+  console.log(req.body);
+  res.redirect('/');
+});
+
+
 
 app.listen(port, () => {
   console.log(`Evernode server running on port: ${port}`);
