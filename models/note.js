ef9c7d08691f8246e4094ca62eb622748686f9a2
Red-French <red@redfrench.net>
2016-02-19 17:03:32 -0600

    add category functionality and views

--- models/note.js
+++ models/note.js
@@ -2,10 +2,13 @@
 
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('Notes',
+module.exports = mongoose.model('Notes',  // model name
   mongoose.Schema({
     title: String,
-    text: String
+    text: String,
+    category: {
+      type: mongoose.Schema.Types.ObjectId,  // category id
+      ref: 'Categories'  // reference Categories model in category.js
+    }
   })
 );
-
--- routes/note.js
+++ routes/note.js
@@ -1,17 +1,23 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();  // Router is built-in middleware and routing functions.
                                   // You can add HTTP method routes (verbs) to it.
 
 const Note = require('../models/note');
-const note = require('../controllers/note');
+
+const ctrl = require('../controllers/note');
 
 router.param('id', (req, res, next, id) => {
-  Note.findById(id, (err, note) => {
-    if (err) throw err;
+  Note
+    .findById(id)
+    .populate('category')
+    .exec((err, note) => {
+      if (err) throw err;
 
-    req.note = note;
-    next();
-  });
+      req.note = note;
+      next();
+    });
 });
 
 // ROUTES (in general, the routing order does not matter; 
@@ -19,12 +25,13 @@ router.param('id', (req, res, next, id) => {
   // so route parameter should come afterwards or, for example,
   // /notes/new would trigger /notes/:id if /notes/:id were
   // first in the waterfall
-router.get('/notes', note.index);  // when '/notes' use 'controller/note.index'
-router.get('/notes/new', note.newNote);  // when '/notes/new' use 
-router.get('/notes/:id', note.show);  // when '/notes/dataIDFromDB' use controller/note.show'
-router.get('/notes/:id/edit', note.edit);
-router.put('/notes/:id', note.update);
-router.delete('/notes/:id', note.destroy);
-router.post('/notes', note.create);
+router.get('/', ctrl.index);
+router.get('/notes', ctrl.index);  // when '/notes' use 'controller/note.index'
+router.get('/notes/new', ctrl.newNote);  // when '/notes/new' use
+router.post('/notes', ctrl.create);
+router.get('/notes/:id', ctrl.show);  // when '/notes/dataIDFromDB' use controller/note.show'
+router.get('/notes/:id/edit', ctrl.edit);
+router.put('/notes/:id', ctrl.update);
+router.delete('/notes/:id', ctrl.destroy);
 
 module.exports = router;
