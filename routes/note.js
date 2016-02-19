f28f7be9a2cb4d13b2081ee078d931f1d76237d3
Red-French <red@redfrench.net>
2016-02-18 22:20:02 -0600

    add comments

--- routes/note.js
+++ routes/note.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router();  // Router is built-in middleware and routing functions.
+                                  // You can add HTTP method routes (verbs) to it.
 
 const Note = require('../models/note');
 const note = require('../controllers/note');
@@ -18,9 +19,9 @@ router.param('id', (req, res, next, id) => {
   // so route parameter should come afterwards or, for example,
   // /notes/new would trigger /notes/:id if /notes/:id were
   // first in the waterfall
-router.get('/notes', note.index);
-router.get('/notes/new', note.newNote);
-router.get('/notes/:id', note.show);
+router.get('/notes', note.index);  // when '/notes' use 'controller/note.index'
+router.get('/notes/new', note.newNote);  // when '/notes/new' use 
+router.get('/notes/:id', note.show);  // when '/notes/dataIDFromDB' use controller/note.show'
 router.get('/notes/:id/edit', note.edit);
 router.put('/notes/:id', note.update);
 router.delete('/notes/:id', note.destroy);
--- server.js
+++ server.js
@@ -9,21 +9,25 @@ const logger = require('./lib/logger');
 const note = require('./routes/note');
 
 const app = express();  // start express
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;  // dynamic port
 
 app.set('view engine', 'jade');
 
+
+// MIDDLEWARE
 app.use(bodyParser.urlencoded({  // for parsing form
   extended: false
 }));
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));  // allow PUT or DELETE if not supported by client
 app.use(logger);
 
 app.get('/', (req, res) => {  // on request, send response
   res.send('Server Running');
 });
 
-app.use(note);
+app.use(note);  // use these routes: routes/note.js
+
+
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
